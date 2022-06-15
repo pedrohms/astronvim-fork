49b705640bc909a993ca67c53950a38bea82f175
Pedro Henrique <pedrohms412@gmail.com>
2022-06-15 18:37:55 -0300

    meu commit inicial

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,7 +1,10 @@
 local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if status_ok then
   treesitter.setup(astronvim.user_plugin_opts("plugins.treesitter", {
-    ensure_installed = {},
+    ensure_installed = { "java", "tsx", "javascript", "html", "vue", "svelte", "astro",
+      "cpp", "css", "scss", "dart", "bash", "dockerfile", "go", "gomod", "graphql",
+      "html", "http", "typescript", "json", "kotlin", "lua", "make", "php", "python",
+      "pug", "prisma", "proto", "regex", "rust", "ruby", "vim", "yaml" },
     sync_install = false,
     ignore_install = {},
     highlight = {
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -281,13 +281,13 @@ local astro_plugins = {
     end,
   },
 
-  -- Smooth scrolling
-  ["declancm/cinnamon.nvim"] = {
-    event = { "BufRead", "BufNewFile" },
-    config = function()
-      require "configs.cinnamon"
-    end,
-  },
+  -- -- Smooth scrolling
+  -- ["declancm/cinnamon.nvim"] = {
+  --   event = { "BufRead", "BufNewFile" },
+  --   config = function()
+  --     require "configs.cinnamon"
+  --   end,
+  -- },
 
   -- Smooth escaping
   ["max397574/better-escape.nvim"] = {
